01bb92532484f9a359e08461107c02ec4385e1b9
oditha14@gmail.com <62847596+odis43@users.noreply.github.com>
2022-01-29 20:41:59 -0500

    initialcommit

--- scripts.js
+++ scripts.js
@@ -53,4 +53,7 @@ var TxtRotate = function(el, toRotate, period) {
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
+    document.querySelector("button.btn").click();
   };
+
+  
